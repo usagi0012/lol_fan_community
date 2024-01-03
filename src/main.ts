570080fcc2e336be20a2cb55791281dfdcd586e8
jiwon <usagi0012@naver.com>
2024-01-03 14:19:43 +0900

    인증 기능

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    //DTO를 사용하기 위해 꼭 들어가야하는 옵션
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
   await app.listen(3000);
 }
+
 bootstrap();
